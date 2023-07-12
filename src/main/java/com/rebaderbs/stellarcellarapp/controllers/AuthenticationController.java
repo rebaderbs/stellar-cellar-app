409298651326f2ee5fa233f0e2937ec42306fb3b
rebaderbs <rderbak@gmail.com>
2023-07-12 12:24:31 -0500

    header fragments working as expected; logout working

--- src/main/java/com/rebaderbs/stellarcellarapp/controllers/AuthenticationController.java
+++ src/main/java/com/rebaderbs/stellarcellarapp/controllers/AuthenticationController.java
@@ -125,7 +125,7 @@ public class AuthenticationController {
     @GetMapping("/logout")
     public String logout(HttpServletRequest request){
         request.getSession().invalidate();
-        return "redirect:/login";
+        return "home";
     }
 
 }
