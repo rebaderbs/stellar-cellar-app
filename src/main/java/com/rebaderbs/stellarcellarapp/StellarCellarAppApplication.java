03cbf127a3b8dfcdc3cf3f6b856e80fddd600f2a
rebaderbs <rderbak@gmail.com>
2023-07-12 09:31:35 -0500

    project files created

---
+++ src/main/java/com/rebaderbs/stellarcellarapp/StellarCellarAppApplication.java
@@ -0,0 +1,13 @@
+package com.rebaderbs.stellarcellarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StellarCellarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StellarCellarAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rebaderbs/stellarcellarapp/StellarCellarAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rebaderbs.stellarcellarapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StellarCellarAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
